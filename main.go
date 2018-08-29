13e2cf25828963eeb31d600a2ab0ae427c3a7e2d
Inhyeok Kim <haruband@gmail.com>
2018-08-29 15:53:03 +0900

    Add devmapper device name option
    Use options from main() instead of Init() from plugin framework

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+)
+
+import (
 	graphhelper "github.com/docker/go-plugins-helpers/graphdriver"
 )
 
@@ -9,7 +14,18 @@ const (
 )
 
 func main() {
-	d, _ := newOverlitDriver()
+	var devname string
+
+	flag.StringVar(&devname, "devname", "", "devmapper device name")
+	flag.Parse()
+
+	options := []string{}
+
+	if devname != "" {
+		options = append(options, fmt.Sprintf("devname=%s", devname))
+	}
+
+	d, _ := newOverlitDriver(options)
 	h := graphhelper.NewHandler(d)
 	h.ServeUnix(sockAddr, 0)
 }
